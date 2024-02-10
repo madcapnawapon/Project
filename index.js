7010c30e50c312f52a2d271aa1965f359362483b
madcapnawapon <nawaponkandam@gmail.com>
2024-02-11 04:39:28 +0700

    กุง่วง

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const flash = require('connect-flash')
 
 const indexController = require('./controllers/indexController')
 const loginController = require('./controllers/loginController')
+const registerController = require('./controllers/registerController')
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs')
 
 app.get('/',indexController)
 app.get('/login',loginController)
+app.get('/register',registerController)
 
 app.listen(4000, ()=> {
     console.log("app listening on port 4000")
