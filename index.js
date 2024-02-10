7a0b24f219edbae1dd74faabe65317c4f77a4e69
madcapnawapon <nawaponkandam@gmail.com>
2024-02-10 22:26:58 +0700

    Database

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const ejs = require('ejs')
 const mongoose = require('mongoose')
 const expressSession = require('express-session')
 const flash = require('connect-flash')
-const indexController = require('./controllers/indexController')
 
-indexController = require('./controllers/indexController')
+
+const indexController = require('./controllers/indexController')
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -14,7 +14,7 @@ app.use(express.urlencoded())
 app.use(flash())
 app.set('view engine', 'ejs')
 
-app.get('/', indexController)
+app.get('/',indexController)
 
 app.listen(4000, ()=> {
     console.log("app listening on port 4000")
