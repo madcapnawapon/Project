8d5d0015a0b6da0919b89ef1ef118641fc7636fb
madcapnawapon <nawaponkandam@gmail.com>
2024-02-11 12:12:58 +0700

    render

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const expressSession = require('express-session')
 const flash = require('connect-flash')
 
 
+mongoose.connect('mongodb+srv://admin:1234@cluster0.dzu6zww.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser:true
+})
+
 const indexController = require('./controllers/indexController')
 const loginController = require('./controllers/loginController')
 const registerController = require('./controllers/registerController')
+const ownerController = require('./controllers/ownerController')
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -19,6 +24,7 @@ app.set('view engine', 'ejs')
 app.get('/',indexController)
 app.get('/login',loginController)
 app.get('/register',registerController)
+app.get('/owner',ownerController)
 
 app.listen(4000, ()=> {
     console.log("app listening on port 4000")
