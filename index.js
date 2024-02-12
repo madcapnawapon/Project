fe68186edfdc97d0fa3d804c1fea15988b76084e
madcapnawapon <nawaponkandam@gmail.com>
2024-02-12 19:13:29 +0700

    loggout

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const expressSession = require('express-session')
 const flash = require('connect-flash')
 
 global.loggedIn = null
+
 mongoose.connect('mongodb+srv://admin:1234@cluster0.dzu6zww.mongodb.net/?retryWrites=true&w=majority',{
     useNewUrlParser:true
 })
@@ -18,7 +19,7 @@ const ownerController = require('./controllers/ownerController')
 const aboutController = require('./controllers/aboutController')
 const storeUserController = require('./controllers/storeUserController')
 const loginUserController = require('./controllers/loginUserController')
-/*const logoutController = require('./controllers/logoutController')*/
+const logout = require('./controllers/logout')
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -29,7 +30,7 @@ app.use(expressSession({
     secret:"node secret"
 }))
 app.use("*",(req,res,next)=>{
-    loggedIn= req.session.userid
+    loggedIn = req.session.userId
     next()
 })
 app.get('/',indexController)
@@ -40,7 +41,7 @@ app.get('/owner',ownerController)
 app.get('/about',aboutController)
 app.post('/user/register',storeUserController)
 app.post('/user/login', loginUserController)
-/*app.get('/logout',logoutController)*/
+app.get('/logout', logout)
 app.listen(4000, ()=> {
     console.log("app listening on port 4000")
 })
